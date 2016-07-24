048faea0a4ccf7aa57ac7d92e2dbd49d1dddd551
xiaobo.huang <xiaobo.huang@hand-china.com>
2016-07-24 23:08:55 +0800

    账户的基本信息验证

--- app/models/user_general.rb
+++ app/models/user_general.rb
@@ -2,5 +2,10 @@ class UserGeneral < ActiveRecord::Base
   self.table_name = "user_generals"
 
   # 一个人只有一套资料
-  validates :user_id,uniqueness: true
+  validates :user_id,uniqueness: true,message: "用户的基本资料已经存在"
+  validates_presence_of :name,message: "姓名不能为空"
+  validates_presence_of :sex,"性别不能为空"
+  validates_presence_of  :mobile_phone,message: "联系方式不能为空"
+  # age只允许为整数且最大要小于140
+  validates :age,numericality: { only_integer: true,less_than: 140 },message: "年龄只能为数字"
 end
