e77a5cd0ddf9a4ad520958de5729739d463027d7
xiaobo.huang <xiaobo.huang@hand-china.com>
2016-08-02 22:28:13 +0800

    重新整理相关chanenl

--- app/jobs/addmessage_job.rb
+++
@@ -1,8 +0,0 @@
-class AddmessageJob < ApplicationJob
-  queue_as :default
-
-  def perform(message)
-    # Do something later
-    ActionCable.server.broadcast "room:#{message.channel_id}:comments", comment: message.content
-  end
-end
